675c70f396b0422f891fc04d2b1ba042b979aa1f
Can Vural <can9119@gmail.com>
2019-03-12 10:59:32 +0100

    wip

--- src/Serialization.js
+++ src/Serialization.js
@@ -1,8 +1,7 @@
-class Serialization {
+export class Serialization {
 	stringify(obj) {
 		return JSON.stringify(obj, function(key, value) {
-			if (value instanceof Function || typeof value == 'function')
-				return '__func__:' + value.toString();
+			if (value instanceof Function || typeof value == 'function') return '__func__:' + value.toString();
 			if (value instanceof RegExp) return '__regex__:' + value;
 			return value;
 		});
@@ -11,13 +10,9 @@ class Serialization {
 	parse(str) {
 		return JSON.parse(str, function(key, value) {
 			if (typeof value != 'string') return value;
-			if (value.lastIndexOf('__func__:', 0) === 0)
-				return eval('(' + value.slice(9) + ')');
-			if (value.lastIndexOf('__regex__:', 0) === 0)
-				return eval('(' + value.slice(10) + ')');
+			if (value.lastIndexOf('__func__:', 0) === 0) return eval('(' + value.slice(9) + ')');
+			if (value.lastIndexOf('__regex__:', 0) === 0) return eval('(' + value.slice(10) + ')');
 			return value;
 		});
 	}
 }
-
-export default Serialization;
